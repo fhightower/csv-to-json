a9af746615cfaf7cefe8512da3dbd4ba9bb72151
Floyd Hightower <floyd.hightower27@gmail.com>
2018-08-13 17:58:44 -0400

    Improving interface with convert function

--- csv_to_json/__init__.py
+++ csv_to_json/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from .csv_to_json import convert
+
 __author__ = """Floyd Hightower"""
 __email__ = 'floyd.hightower27@gmail.com'
 __version__ = '0.1.0'
